8e2874c97683830a915260414b1b76c3880a6783
Slim-Bob <112772549+Slim-Bob@users.noreply.github.com>
2023-01-19 01:43:57 +0500

    adapter for class

--- Adapter/adapter_2.py
+++ Adapter/adapter_2.py
@@ -1,7 +1,3 @@
-# Пример структурного шаблона проектирования программ «Адаптер», предназначенный для организации использования функций
-# объекта, недоступного для модификации, через специально созданный интерфейс. Другими словами, он позволяет объектам
-# с несовместимыми интерфейсами работать вместе.
-
 from __future__ import annotations
 from abc import ABCMeta, abstractmethod
 
@@ -28,24 +24,23 @@ class ParrotsScales:
         return self._parrots
 
 
-class AdapterForParrotsScales(IScale):
+class AdapterForParrotsScales(ParrotsScales, IScale):
     # Лично у меня попугай ассоциируется с видом Какаду.
     # Запрос в поисковой системе выдал длину от 30 до 60 см
     # Берем среднее (45 см) и принимаем полученное значение за коэффициент
     COEFFICIENT_CM: float = 45
 
-    def __init__(self, parrots_scales: ParrotsScales):
-        self._parrots_scales = parrots_scales
+    def __init__(self, parrots: float):
+        super().__init__(parrots)
 
     def get_length(self) -> float:
-        return self._parrots_scales.get_parrots() * AdapterForParrotsScales.COEFFICIENT_CM
+        return super().get_parrots() * AdapterForParrotsScales.COEFFICIENT_CM
 
 
 if __name__ == '__main__':
     parrots: float = 2.5  # Два с половиной попугая
 
-    pScales = ParrotsScales(parrots)
-    aParrotsScales = AdapterForParrotsScales(pScales)
+    aParrotsScales = AdapterForParrotsScales(parrots)
 
     cm = aParrotsScales.get_length()
     print(f'{parrots} попугая равняется {cm} см')
